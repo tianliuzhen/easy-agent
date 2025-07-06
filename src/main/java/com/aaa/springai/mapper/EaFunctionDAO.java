2657b2ac0280e09707afeb94a7df6e1b81819370
tianliuzhen <tlzallen@163.com>
2025-07-06 22:23:05 +0800

    add db function

---
+++ src/main/java/com/aaa/springai/mapper/EaFunctionDAO.java
@@ -0,0 +1,7 @@
+package com.aaa.springai.mapper;
+
+import com.aaa.springai.domain.DO.EaFunctionDO;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface EaFunctionDAO extends Mapper<EaFunctionDO> {
+}
